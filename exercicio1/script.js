ed3c6954c718f72ef2c91cb57ebd2da134aa4ecd
CamiFranca <camillaartc@gmail.com>
2022-09-14 20:03:35 -0300

    eitcha que foi dia

---
+++ exercicio1/script.js
@@ -0,0 +1,26 @@
+
+/*function pessoa (nome){
+console.log('Olá, ' + nome)
+
+}
+pessoa('Camilla')
+
+function tabuada (num1){
+    for(let i = 1; i<=10; i++){
+    console.log(i*num1)
+    }
+}
+tabuada(6)*/
+
+const tabuada = (num1)=>{
+    for(let i = 1; i<=10; i++){
+    console.log(i*num1)
+    }
+}
+tabuada(6)
+
+const pessoa = function (nome){
+    console.log('Olá, ' + nome)
+    
+    }
+    pessoa('Camilla')
